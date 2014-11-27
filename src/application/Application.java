dda03bcb784ef9408a102c648d0bf521cd02d42b
usuario <usuario@10.22.143.28>
2014-11-27 12:43:12 +0000

    implementación mock 27.11.14

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,10 +1,12 @@
 package application;
 
+import control.ExchangeOperation;
 import model.Currency;
+import model.CurrencySet;
 import persistance.CurrencySetLoader;
 
 public class Application {
     public static void main(String[] args){
+        new ExchangeOperation().execute(new CurrencySetLoader().load());
     }
-    
 }
--- src/control/ExchangeOperation.java
+++ src/control/ExchangeOperation.java
@@ -1,5 +1,13 @@
 package control;
 
+import model.CurrencySet;
+
 public class ExchangeOperation {
+
+    public ExchangeOperation() {
+    }
     
+    public void execute(CurrencySet currencySet){
+        System.out.println("he llegado hasta aquí");
+    }
 }
